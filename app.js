3da9ffaa17b4263c26c3d4be16bd870fc7853894
taruntanmay <tanmaytarun536@gmail.com>
2019-09-25 18:30:26 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const UserScore=0;
+const CompScore=0;
+const userScore_span =document.getElementById("user-score");
+const compScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissor_div = document.getElementById("s");
+
+
+rock_div.addEventListener('click',function(){
+    console.log("hey you clicked on rock");
+})
+paper_div.addEventListener('click',function(){
+    console.log("hey you clicked on paper");
+})
+scissor_div.addEventListener('click',function(){
+    console.log("hey you clicked on scissor");
+})
