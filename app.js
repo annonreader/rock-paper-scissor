cb301910a4abeec61f3ed19ab2bb5abe7ea0e85f
taruntanmay <tanmaytarun536@gmail.com>
2019-09-26 10:34:12 +0530

    final commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const UserScore=0;
-const CompScore=0;
+let UserScore=0;
+let CompScore=0;
 const userScore_span =document.getElementById("user-score");
 const compScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > h1");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissor_div = document.getElementById("s");
@@ -16,39 +16,56 @@ function getComputerChoice(){
 
 function game(userChoice)
 {
-     const computerChoice = getComputerChoice();
-     //console.log(getComputerChoice);
+     let computerChoice = getComputerChoice();
+     console.log("i am logging userchoice "+userChoice);
      switch(userChoice+computerChoice)
      {
-         case 'rs':
-         case 'sp':
-         case 'pr':
-             win();
+         case "rs":
+         case "sp":
+         case "pr":
+             win(userChoice,computerChoice);
+            // console.log("i am logging the user choice--> "+userChoice);
              break;
-         case 'rp':
-         case 'ps':
-         case 'sr':
+         case "rp":
+         case "ps":
+         case "sr":
             lose();
             break;
-         case 'rr':
-         case 'pp':
-         case 'ss':
+         case "rr":
+         case "pp":
+         case "ss":
             draw();
              break;
      }
 }
 
-function win()
+function convertToWord(letter)
 {
+    if(letter == "r") return "Rock";
+    if(letter == "p") return "Paper";
+    return "Scissor";
+}
+function win(userChoice, computerChoice)
+{
+    UserScore++;
+    userScore_span.innerHTML = UserScore;
+    compScore_span.innerHTML = CompScore;
+    result_p.innerHTML = convertToWord(userChoice)+ " beats " +convertToWord(computerChoice)+", You Win! ";
     console.log('win');
 }
-function lose()
+function lose(userChoice,computerChoice)
 {
+    CompScore++;
+    userScore_span.innerHTML = UserScore;
+    compScore_span.innerHTML = CompScore;
+    result_p.innerHTML = convertToWord(userChoice)+ " beats " +convertToWord(computerChoice)+ ", You Lose! ";
+    console.log('win');
     console.log('lose');
 }
-function draw()
+function draw(userChoice,computerChoice)
 {
     console.log('draw');
+    result_p.innerHTML = convertToWord(userChoice)+ " beats " +convertToWord(computerChoice)+ ", Draw! ";
 }
 
 
@@ -59,15 +76,15 @@ function draw()
 function main(){
 rock_div.addEventListener('click',function(){
     //console.log("hey you clicked on rock");
-    game('r');
+    game("r");
 })
 paper_div.addEventListener('click',function(){
   //  console.log("hey you clicked on paper");
-    game('p');
+    game("p");
 })
 scissor_div.addEventListener('click',function(){
    // console.log("hey you clicked on scissor");
-    game('s');
+    game("s");
 })
 }
 
